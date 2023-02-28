ca07eafb955899ff87157842e2701a420aac94fd
mingdev <mingdev@163.com>
2023-02-28 06:18:57 +0000

    use lodash merge object

--- src/ImageConfigWebpackPlugin.js
+++ src/ImageConfigWebpackPlugin.js
@@ -1,29 +1,35 @@
+const mergeFn = require('lodash/merge');
 const config = require('#config');
-
-const defaultOptions = (isDevelopment) => ({
-  suffix: ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp', 'avif'],
-  parser: {
-    dataUrlCondition: {
-      maxSize: isDevelopment ? undefined : 1000,
-    },
-  },
-  generator: {
-    filename: isDevelopment ? '[path][name][ext]' : 'static/images/[contenthash:10][ext]',
-  },
-});
 
 module.exports = class ImageConfigWebpackPlugin {
   constructor(options) {
     this.options = options;
+
+    this.getOptions = this.getOptions.bind(this);
   }
 
   apply(compiler) {
-    const cfg = config({
-      ...defaultOptions(compiler.options.mode === 'development'),
-      ...this.options,
-    });
+    const { constructor, getOptions } = this;
+
+    const cfg = config(getOptions(compiler));
     // Merge config
-    compiler.hooks.afterEnvironment.tap('ImageConfigWebpackPlugin', () => compiler.options.module.rules.push(...cfg.module.rules));
+    compiler.hooks.afterEnvironment.tap(constructor.name, () => compiler.options.module.rules.push(...cfg.module.rules));
     cfg.plugins.forEach((plugin) => plugin.apply(compiler));
+  }
+
+  getOptions(compiler) {
+    const isDevelopment = compiler.options.mode === 'development';
+    const defaultOpt = {
+      suffix: ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp', 'avif'],
+      parser: {
+        dataUrlCondition: {
+          maxSize: isDevelopment ? undefined : 1000,
+        },
+      },
+      generator: {
+        filename: isDevelopment ? '[path][name][ext]' : 'static/images/[contenthash:10][ext]',
+      },
+    };
+    return mergeFn({}, defaultOpt, this.options);
   }
 };
